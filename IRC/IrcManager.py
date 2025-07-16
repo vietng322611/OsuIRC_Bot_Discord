c2b7cfac70f2137f2efe006ecc8a1653c144cb86
vietng322611 <vietng322611@gmail.com>
2025-07-16 09:25:23 +0700

    Ref: Rename `Manager` to `IrcManager` for clairity

--- IRC/IrcManager.py
+++ IRC/IrcManager.py
@@ -1,10 +1,11 @@
 from collections import defaultdict
 from .Channel import Channel
+from typing import Any
 
 import logging
-class Manager:
+class IrcManager:
     def __init__(self):
-        self.logger                        = logging.getLogger('Manager')
+        self.logger                        = logging.getLogger('IrcManager')
         self.chat_list: dict[str, Channel] = defaultdict()
         self.nick                          = ""
         self.passw                         = ""
@@ -35,18 +36,29 @@ class Manager:
         try:
             match (data[0]):
                 case "0": # join
-                    if data[2] == self.nick: self.add_chat(name)
+                    if data[2] == self.nick:
+                        self.add_chat(name)
                     self.chat_list[name].add_user(data[2:len(data)])
 
                 case "1": # message
-                    if name == self.nick: name = data[2]
-                    if not self.chat_list[name]: self.add_chat(name)
+                    if name == self.nick:
+                        name = data[2]
+                    if not self.chat_list.get(name):
+                        self.add_chat(name)
                     self.chat_list[name].update(data)
 
                 case "2": # leave
-                    if (data[2] == self.nick): self.remove_chat(name)
-                    else: self.chat_list[name].remove_user(data[2])
+                    if data[2] == self.nick:
+                        self.remove_chat(name)
+                    else:
+                        self.chat_list[name].remove_user(data[2])
         except Exception as e:
-            self.logger.debug("Unknown error: " + str(e))
+            self.logger.error(e, exc_info=True)
 
-manager = Manager()
+    async def listen_for_pattern(self, chat_name: str, pattern: str) -> tuple[str | Any, ...]:
+        chat = self.get_chat(chat_name)
+        if not chat:
+            return tuple()
+        return await chat.listen_for_pattern(pattern)
+
+ircManager = IrcManager()
